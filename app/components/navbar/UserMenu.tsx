c2cbb86e01d73d5711148dcf05e15d9663f8f884
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-04-02 12:44:29 -0500

    adding Menu Item and toogle function

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,7 +1,14 @@
 'use client'
+import {useState, useCallback} from 'react'
 import {AiOutlineMenu} from 'react-icons/ai'
 import Avatar from '../Avatar';
+import MenuItem from './MenuItem';
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState
+  (false);
+  const toggleOpen = useCallback(()=>{
+    setIsOpen(value=>!value)
+  },[])
   return (
   <div className="relative">
     <div className="flex flex-row items-center gap-3">
@@ -13,7 +20,7 @@ const UserMenu = () => {
       >
         Airbnb your home
       </div>
-      <div onClick={()=>{console.log('Hola mundo');}}
+      <div onClick={toggleOpen}
       className="py-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition "
       >
         <AiOutlineMenu />
@@ -22,6 +29,18 @@ const UserMenu = () => {
         </div>
       </div>
     </div>
+    {
+      isOpen && (
+        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+          <div className='flex flex-col cursor-pointer'>
+            <>
+              <MenuItem onClick={()=>{}} label='Login'/>
+              <MenuItem onClick={()=>{}} label='Sign up'/>
+            </>
+          </div>
+        </div>
+      )
+    }
   </div> );
 }
  
