b4bf534b29730b84542dd95ee4cdf7d44e86b0cc
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-04-20 17:38:16 -0500

    fix the erros in component called ContrySelect

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,67 +1,63 @@
-'use client'
-import useCountries from '@/app/hooks/useCountries'
+'use client';
+
 import Select from 'react-select'
 
-export type CountrySelectValue={
-  flag:string
-  label:string
-  latlng:number[]
-  region:string
-  value:string
-}
-interface CountrySelectProps{
-  value?:CountrySelectProps;
-  onChange:(value:CountrySelectValue)=>void;
+import useCountries from '@/app/hooks/useCountries';
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[],
+  region: string;
+  value: string
 }
 
-const CountrySelect:React.FC<CountrySelectProps> = ({
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange: (value: CountrySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({
   value,
   onChange
 }) => {
-
-  const {getAll} = useCountries();
+  const { getAll } = useCountries();
 
   return ( 
-    <div
-      className="
-      
-      "
-    >
-      <Select 
+    <div>
+      <Select
         placeholder="Anywhere"
         isClearable
-        options= {getAll()}
+        options={getAll()}
         value={value}
-        onChange={(value:any)=>onChange(value as CountrySelectValue)
-        }
-        formatOptionLabel={(option:any)=>(
-          <div className='flex flex-row items-center gap-3' >
+        onChange={(value) => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div className="
+          flex flex-row items-center gap-3">
             <div>{option.flag}</div>
             <div>
               {option.label},
-              <span className='text-neutral-500 ml-1' >{option.region}</span>
-            </div> 
-            
+              <span className="text-neutral-500 ml-1">
+                {option.region}
+              </span>
+            </div>
           </div>
         )}
-
         classNames={{
-          control:()=>'p-3 border-2',
-          input:()=>'text-lg',
-          option:()=>'text-lg'
+          control: () => 'p-3 border-2',
+          input: () => 'text-lg',
+          option: () => 'text-lg'
         }}
-        theme={(theme:any)=>({
+        theme={(theme) => ({
           ...theme,
-          borderRadius:6,
-          colors:{
+          borderRadius: 6,
+          colors: {
             ...theme.colors,
-            primary:'black',
-            primary25:'#ffe4e6'
+            primary: 'black',
+            primary25: '#ffe4e6'
           }
         })}
-
       />
-
     </div>
    );
 }
